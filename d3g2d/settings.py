47ee149e93d2071a6062b096fb4346a48b0853c1
Humna Awan <humna.awan@rutgers.edu>
2019-07-20 20:39:39 -0700

    Added plotting params.

--- d3g2d/settings.py
+++ d3g2d/settings.py
@@ -1,1 +1,20 @@
 h0 = 0.704
+
+# plotting params
+fontsize = 18
+rcparams = {}
+rcparams['figure.figsize'] = (10, 6)
+rcparams['axes.labelsize'] = fontsize
+rcparams['legend.fontsize'] = fontsize-4
+rcparams['axes.titlesize'] = fontsize
+rcparams['axes.linewidth'] = 2
+rcparams['axes.grid'] = True
+for axis in ['x', 'y']:
+    rcparams['%stick.labelsize'%axis] = fontsize-2
+    rcparams['%stick.direction'%axis] = 'in'
+    rcparams['%stick.major.size'%axis] = 5.5
+    rcparams['%stick.minor.size'%axis] =  3.5
+    rcparams['%stick.major.width'%axis] = 2
+    rcparams['%stick.minor.width'%axis] = 1.5
+rcparams['xtick.top'] = True
+rcparams['ytick.right'] = True

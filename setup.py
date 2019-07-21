6b8d38b68477b11e632c3466135f8f9b43ed3bfa
Humna Awan <humna.awan@rutgers.edu>
2019-07-20 18:40:04 -0700

    Set up to help import code.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import os
+
+setup(name='d3g2d',
+      version='0.1',
+      packages=['d3g2d'],
+      package_dir={'d3g2d': 'd3g2d'},
+      url= 'https://github.com/humnaawan/3D-galaxies-kavli',
+      author= 'Humna Awan',
+      author_email= 'humna.awan@rutgers.edu'
+     )

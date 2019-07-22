274d7157716ff2e26ccb3a2fc88fbdaae7d7df8a
Humna Awan <humna.awan@rutgers.edu>
2019-07-22 00:54:42 -0700

    Run script to get shapes for all TNG cutouts.

---
+++ runscripts/bash_get_tng_shapes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+data_dir='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/tng-100/'
+
+python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_get_shapes.py \
+                --data_dir=${data_dir}

e011809295095f231c3e51246a08e7cedbe95faf
Humna Awan <humna.awan@rutgers.edu>
2019-07-23 00:51:31 -0700

    Updated summary table path.

--- d3g2d/settings.py
+++ d3g2d/settings.py
@@ -2,7 +2,7 @@ h0 = 0.704
 illustris_snap2z = {'z': 0.0, 'snap': 135}
 tng_snap2z = {'z': 0.4, 'snap': 72}
 
-summary_datapath = '/Users/humnaawan/repos/3D-galaxies-kavli/data/tng_2d_summary/'
+summary_datapath = '/Users/humnaawan/repos/3D-galaxies-kavli/data/tng_lowres_2d_summary/'
 
 # plotting params
 fontsize = 18

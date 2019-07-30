bb9b80f8446008026a310834742e9e1ad2eceb7e
Humna Awan <humna.awan@rutgers.edu>
2019-07-29 19:14:06 -0700

    Added code plot some figs for summary data.

--- runscripts/get_features.py
+++ runscripts/get_features.py
@@ -1,8 +1,12 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.lines import Line2D
 import pandas as pd
 import numpy as np
-import pickle
-import datetime, time, socket, os
-from d3g2d import run_rf, readme as readme_obj, get_time_passed
+import pickle, datetime, time, socket, os
+
+from d3g2d import run_rf, readme as readme_obj, get_time_passed, rcparams
+for key in rcparams: mpl.rcParams[key] = rcparams[key]
 # ------------------------------------------------------------------------------
 from optparse import OptionParser
 parser = OptionParser()
@@ -29,7 +33,9 @@ update += 'Running get_features.py on %s\n\n' % socket.gethostname()
 update += 'Options:\n%s\n' % options
 readme = readme_obj(outdir=outdir, readme_tag=readme_tag, first_update=update)
 readme.run()
-
+# set up the figs directory
+fig_dir = '%s/figs/' % outdir
+os.makedirs(fig_dir, exist_ok=True)
 # start things up
 start_time = time.time()
 # ------------------------------------------------------------------------------
@@ -77,10 +83,11 @@ def get_features(data_for_halo):
 # read in shape data to get the haloIds
 file = [ f for f in os.listdir(shape_datapath) if f.startswith('shape100_')][0]
 with open('%s/%s' % (shape_datapath, file), 'rb') as f:
-    shape = pickle.load(f)
+    shape_data = pickle.load(f)
 # ------------------------------------------------------------------------------
 # now read the summary data and assemble features
-for i, haloId in enumerate(shape['haloId']):
+# follow the same order are haloId in the shape data for easy matching later on
+for i, haloId in enumerate(shape_data['haloId']):
     filename = [f for f in os.listdir(summary_datapath) if f.__contains__('_%s_'%haloId)]
     if len(filename) != 1:
         print(filename, haloId)
@@ -94,12 +101,195 @@ for i, haloId in enumerate(shape['haloId']):
         feats = list(out.values())
     else:
         feats = np.vstack([feats, list(out.values())])
+
+    # --------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
+    # plot some things for just one galaxy
+    if i == 10:
+        # ----------------------------------------------------------------------
+        # plot the mass porfiles
+        plt.clf()
+        plt.plot(data['rpix_shape'] * 5.333, data['mu_shape'], 'o-', label='mu_shape')
+        plt.plot(data['aper_rkpc'], 10 ** data['aper_logms'], 'o-', label='aper_logms')
+        plt.plot(data['rpix_shape'] * 5.333, data['maper_shape'], 'o-', label='maper_shape')
+        plt.plot(data['rpix_prof'] * 5.333, data['maper_prof'], 'x-', label='maper_prof')
+        plt.plot(data['rpix_prof'] * 5.333, data['mu_prof'], 'x-', label='mu_prof')
+        # plot details
+        plt.legend(bbox_to_anchor=(1,1))
+        plt.xlabel('kpc')
+        plt.ylabel(r'$\mu$')
+        plt.gca().set_xscale('log')
+        plt.gca().set_yscale('log')
+        # save plot
+        filename = 'mass_profile_halo%s.png' % haloId
+        plt.savefig('%s/%s'%(fig_dir, filename), format='png',
+                    bbox_inches='tight')
+        plt.close('all')
+        readme.update(to_write='Saved %s\n' % filename)
+    # --------------------------------------------------------------------------
 # assemble the features as a dataframe
 feats = pd.DataFrame(feats, columns=keys)
 # save the data
 filename = 'features_%s.csv' % len( feats.keys() )
 feats.to_csv('%s/%s' % (outdir, filename), index=False)
-update = 'Saved %s in %s\n' % (filename, outdir)
-update += '## Time taken: %s\n'%get_time_passed(start_time)
-readme.update(to_write=update)
+readme.update(to_write='Saved %s in %s\n' % (filename, outdir))
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# plot some more plots
+# ----------------------------------------------------------------------
+# plot all the mass profiles and color-code with shape
+colors = {'P': 'r', 'O': 'b', 'S': 'k', 'T': 'c'}
+counters = {}
+for key in colors: counters[key] = 0
+
+plt.clf()
+for i, haloId in enumerate(shape_data['haloId']):
+    filename = [f for f in os.listdir(summary_datapath) if f.__contains__('_%s_'%haloId)]
+    if len(filename) != 1:
+        print(filename, haloId)
+        break
+    else:
+        filename = filename[0]
+    data = np.load('%s/%s' % (summary_datapath, filename))
+    # find the shape
+    shape_class = np.array(shape_data['shape'])[i][0]
+    # add to the right counter
+    counters[shape_class] += 1
+    # plot
+    plt.plot(( data['rpix_prof'] * 5.333), np.log10(data['mu_prof']), '.-', alpha=0.3, color=colors[shape_class])
+# plot details
+plt.xlabel('R (kpc)' )
+plt.ylabel(r'log10($\mu_{prof}$) ')
+plt.xlim(50, 1000)
+plt.ylim(0, 10)
+# add legend
+custom_lines = [Line2D([0], [0], color=colors['P'], lw=10),
+                Line2D([0], [0], color=colors['O'], lw=10),
+                Line2D([0], [0], color=colors['T'], lw=10),
+                Line2D([0], [0], color=colors['S'], lw=10)]
+plt.legend(custom_lines,
+           ['Prolate (%s)' % counters['P'],
+            'Oblate (%s)' % counters['O'],
+            'Triaxial (%s)' % counters['T'],
+            'Spherical (%s)' % counters['S']],
+           bbox_to_anchor=(1, 1), frameon=True)
+# save plot
+filename = 'mass_profiles_%sgals.png' % (i+1)
+plt.savefig('%s/%s'%(fig_dir, filename), format='png',
+            bbox_inches='tight')
+plt.close('all')
+readme.update(to_write='Saved %s\n' % filename)
+# ----------------------------------------------------------------------
+# now redo the plot above + include subplots for various quartiles in logM100
+for key in colors: counters[key] = 0
+
+plt.clf()
+nrows, ncols = 5, 1
+fig, axes = plt.subplots(nrows, ncols)
+plt.subplots_adjust(wspace=0.3, hspace=0.3, top=0.9)
+
+logm100s = feats['logm100'].values
+counter_1, counter_2, counter_3, counter_4 = 0, 0, 0, 0
+for i, haloId in enumerate(shape_data['haloId']):
+    filename = [f for f in os.listdir(summary_datapath) if f.__contains__('_%s_'%haloId)][0]
+    data = np.load('%s/%s' % (summary_datapath, filename))
+
+    # find the shape
+    shape_class = np.array(shape_data['shape'])[i][0]
+    # add to the right counter
+    counters[shape_class] += 1
+    # plot; all masses
+    axes[0].plot(( data['rpix_prof'] * 5.333), np.log10(data['mu_prof']), '.-', alpha=0.3, color=colors[shape_class])
+
+    # now determine where the logM100 of this galaxies lies in the overall distribution.
+    # consider each quartile separately
+    q1, q2 = 0, 0.25
+    if ( logm100s[i] >= np.quantile(logm100s, q1)) and ( logm100s[i] < np.quantile(logm100s, q2) ):
+        axis_ind = 1
+        counter_1 += 1
+    q1, q2 = 0.25, 0.5
+    if ( logm100s[i] >= np.quantile(logm100s, q1)) and ( logm100s[i] < np.quantile(logm100s, q2) ):
+        axis_ind = 2
+        counter_2 += 1
+    q1, q2 = 0.5, 0.75
+    if ( logm100s[i] >= np.quantile(logm100s, q1)) and ( logm100s[i] < np.quantile(logm100s, q2) ):
+        axis_ind = 3
+        counter_3 += 1
+    q1, q2 = 0.75, 1.0
+    if ( logm100s[i] >= np.quantile(logm100s, q1)) and ( logm100s[i] <= np.quantile(logm100s, q2) ):
+        axis_ind = 4
+        counter_4 += 1
+    # now plot
+    axes[axis_ind].plot(( data['rpix_prof'] * 5.333), np.log10(data['mu_prof']), '.-',
+                        alpha=0.3, color=colors[shape_class])
+# plot details
+fig.set_size_inches(15, 15)
+axes[4].set_xlabel('R (kpc)' )
+for row in range( nrows ):
+    axes[row].set_ylabel(r'log10($\mu_{\mathrm{prof}}$) ')
+    axes[row].set_xlim(50, 1000)
+    axes[row].set_ylim(0, 10)
+# titles
+axes[0].set_title('all masses (%s galaxies)' % ( counter_1 + counter_2 + counter_3 + counter_4))
+q1, q2 = 0, 0.25
+axes[1].set_title(r' %.2f $\leq$ logM100 < %.2f (%s galaxies)' % (np.quantile(logm100s, q1),
+                                                                  np.quantile(logm100s, q2),
+                                                                  counter_1))
+q1, q2 = 0.25, 0.5
+axes[2].set_title(r' %.2f $\leq$ logM100 < %.2f (%s galaxies)' % (np.quantile(logm100s, q1),
+                                                                  np.quantile(logm100s, q2),
+                                                                  counter_2))
+q1, q2 = 0.5, 0.75
+axes[3].set_title(r' %.2f $\leq$ logM100 < %.2f (%s galaxies)' % (np.quantile(logm100s, q1),
+                                                                  np.quantile(logm100s, q2),
+                                                                  counter_3))
+q1, q2 = 0.75, 1.0
+axes[4].set_title(r' %.2f $\leq$ logM100 $\leq$ %.2f (%s galaxies)' % (np.quantile(logm100s, q1),
+                                                                  np.quantile(logm100s, q2),
+                                                                  counter_4))
+# add legend
+axes[0].legend(custom_lines,
+               ['Prolate (%s)' % counters['P'],
+                'Oblate (%s)' % counters['O'],
+                'Triaxial (%s)' % counters['T'],
+                'Spherical (%s)' % counters['S']],
+               bbox_to_anchor=(1, 1), frameon=True)
+# save plot
+filename = 'mass_profiles_%sgals_wm100quartiles.png' % (i+1)
+plt.savefig('%s/%s'%(fig_dir, filename), format='png',
+            bbox_inches='tight')
+plt.close('all')
+readme.update(to_write='Saved %s\n' % filename)
+# ----------------------------------------------------------------------
+# plot the analog of Hongyu's Fig 3.
+m_arr = np.arange( min(logm100s) - 0.1, max(logm100s) + 0.2, 0.1 )
+# plot normed densities
+ind1 = np.where( shape_data['shape'] == 'P' )[0]
+plt.hist(logm100s[ind1], histtype='step', lw=2, bins=m_arr, color='r',
+         label='P normed density (N=%s)' % len(ind1), density=True )
+ind2 = np.where( shape_data['shape'] != 'P')[0]
+plt.hist(logm100s[ind2], histtype='step', bins=m_arr, color='b',
+         lw=2, label='Not-P normed density (N=%s)' % len(ind2), density=True )
+# plot fraction
+frac, ms = [], []
+m_arr = np.arange( min(logm100s), max(logm100s) + 0.2, 0.2 )
+for j in range(len(m_arr) - 1):
+    mlow, mupp = m_arr[j], m_arr[j+1]
+    ind = np.where( ( logm100s >= mlow ) & ( logm100s < mupp ))[0]
+    frac.append( len( set(ind1) & set(ind) ) / len( ind) )
+    ms.append( np.median( [mlow, mupp ]) )
+plt.plot(ms, frac, 'k.-', label='Prolate Fraction')
+# details
+plt.legend()
+plt.xlabel( 'logM100' )
+# save plot
+filename = 'hongyu_analog_%sgals.png' % (i+1)
+plt.savefig('%s/%s'%(fig_dir, filename), format='png',
+            bbox_inches='tight')
+plt.close('all')
+readme.update(to_write='Saved %s\n' % filename)
+
+# ----------------------------------------------------------------------
+readme.update(to_write='## Time taken: %s\n'%get_time_passed(start_time))
 if not quiet: print(update)

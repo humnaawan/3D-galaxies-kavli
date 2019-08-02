518c885b8190f5b07a67f142786c6787e9e01806
Humna Awan <humna.awan@rutgers.edu>
2019-08-02 14:29:05 -0700

    Updated haloIds for illustris data.

--- runscripts/get_illustris_data.py
+++ runscripts/get_illustris_data.py
@@ -1,11 +1,15 @@
-import datetime, time, socket
+import datetime, time, socket, os
 from d3g2d import get_data, readme as readme_obj, illustris_snap2z
 
 # ------------------------------------------------------------------------------
+summary_datapath = '/Users/humnaawan/repos/3D-galaxies-kavli/data/illustris_mass_shape/mass-all-11p0/'
 outdir = '/Users/humnaawan/repos/3D-galaxies-kavli/outputs/test_illustris/'
 run_name = 'Illustris-1'
 z = illustris_snap2z['z']
-haloIDs = [16941, 5]
+#haloIDs = [16941, 5]
+haloIDs = []
+for f in [f for f in os.listdir(summary_datapath) if f.endswith( '.dat' )]:
+    haloIDs.append( int( f.split('subhalo')[1].split('.dat')[0] ) )
 # ------------------------------------------------------------------------------
 # set up the readme
 start_time = time.time()
@@ -14,7 +18,7 @@ update = '%s\n' % datetime.datetime.now()
 update += 'Running on %s\n\n' % socket.gethostname()
 update += 'Outdir: %s\n' % outdir
 update += 'For z = %s, run_name = %s\n' % (z, run_name)
-update += 'haloIDs: %s\n' % haloIDs
+update += '%s haloIDs:\n%s\n' % ( len(haloIDs), haloIDs)
 readme = readme_obj(outdir=outdir, readme_tag=readme_tag, first_update=update)
 readme.run()
 

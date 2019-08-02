2193dee49777c2237addd8ed7c8a748826cce444
Humna Awan <humna.awan@rutgers.edu>
2019-08-02 16:52:39 -0700

    Updated bash script to run different cases independently.

--- runscripts/bash_run_rf_highres.sh
+++ runscripts/bash_run_rf_highres.sh
@@ -2,55 +2,86 @@
 shape_datapath='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/tng-100/'
 base_outdir='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/'
 # ------------------------------------------------------------------------------
+allclasses_noa1a4=0
+twoclasses_noa1a4=0
+allclasses_wa1a4=0
+twoclasses_wa1a4=0
+regress_wT_noa1a4=1
+# ------------------------------------------------------------------------------
 # run  analysis for one projection;
 features_file='/Users/humnaawan/repos/3D-galaxies-kavli/data/tng_highres/xy/features_28.csv'
 # no masses included; no a1a4; 3 classes
-outdir=${base_outdir}'rf_tng_highres_we_3classes/'
-# classification
-python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
-                --features_file=${features_file} \
-                --shape_datapath=${shape_datapath} \
-                --outdir=${outdir} --high_res --plot_feature_dists \
-                --no_2nd_order_feats --good_radius_feats
-# regression
-python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
-                --features_file=${features_file} \
-                --shape_datapath=${shape_datapath} \
-                --outdir=${outdir} --high_res \
-                --no_2nd_order_feats --good_radius_feats --regress
+if [ $allclasses_noa1a4 == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_3classes/'
+    # classification
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res --plot_feature_dists \
+                    --no_2nd_order_feats --good_radius_feats
+    # regression
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --no_2nd_order_feats --good_radius_feats --regress
+fi
 # ------------------------------------------------------------------------------
 # 2 classes; no masses included; no a1a4
-outdir=${base_outdir}'rf_tng_highres_we_2classes/'
-# classification
-python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
-                --features_file=${features_file} \
-                --shape_datapath=${shape_datapath} \
-                --outdir=${outdir} --high_res \
-                --no_2nd_order_feats --good_radius_feats \
-                --prolate_vs_not
+if [ $twoclasses_noa1a4 == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_2classes/'
+    # classification
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --no_2nd_order_feats --good_radius_feats \
+                    --prolate_vs_not
+fi
 # ------------------------------------------------------------------------------
 # now include a1a4
 # no masses included; 3 classes
-outdir=${base_outdir}'rf_tng_highres_we_3classes_wa1a4/'
-# classification
-python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
-                --features_file=${features_file} \
-                --shape_datapath=${shape_datapath} \
-                --outdir=${outdir} --high_res --plot_feature_dists \
-                --good_radius_feats
-# regression
-python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
-                --features_file=${features_file} \
-                --shape_datapath=${shape_datapath} \
-                --outdir=${outdir} --high_res \
-                --good_radius_feats --regress
+if [ $allclasses_wa1a4 == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_3classes_wa1a4/'
+    # classification
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res --plot_feature_dists \
+                    --good_radius_feats
+    # regression
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --good_radius_feats --regress
+fi
 # ------------------------------------------------------------------------------
 # 2 classes; no masses included
-outdir=${base_outdir}'rf_tng_highres_we_2classes_wa1a4/'
-# classification
-python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
-                --features_file=${features_file} \
-                --shape_datapath=${shape_datapath} \
-                --outdir=${outdir} --high_res \
-                --good_radius_feats \
-                --prolate_vs_not
+if [ $twoclasses_wa1a4 == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_2classes_wa1a4/'
+    # classification
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --good_radius_feats \
+                    --prolate_vs_not
+fi
+# ------------------------------------------------------------------------------
+# regress with triaxiality; no masses; no a1a4
+if [ $regress_wT_noa1a4 == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_3classes_Tonly/'
+    # regression
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --no_2nd_order_feats --good_radius_feats --regress \
+                    --just_triaxiality
+fi

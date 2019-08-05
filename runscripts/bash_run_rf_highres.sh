3e8ab4b5229c37833d45e6f5a59ac01a0b868c24
Humna Awan <humna.awan@rutgers.edu>
2019-08-04 21:46:57 -0700

    Added new run cases.

--- runscripts/bash_run_rf_highres.sh
+++ runscripts/bash_run_rf_highres.sh
@@ -6,10 +6,14 @@ allclasses_noa1a4=0
 twoclasses_noa1a4=0
 allclasses_wa1a4=0
 twoclasses_wa1a4=0
-regress_wT_noa1a4=0
-classify_wT_noa1a4=0
-regress_wT_wa1a4=1
-classify_wT_wa1a4=1
+regress_wT_noa1a4=1
+classify_wT_noa1a4=1
+regress_wT_wa1a4=0
+classify_wT_wa1a4=0
+allclasses_noa1a4_wtsne=0
+twoclasses_noa1a4_wtsne=0
+regress_wT_noa1a4_wtsne=0
+classify_wT_noa1a4_wtsne=0
 # ------------------------------------------------------------------------------
 # run  analysis for one projection;
 features_file='/Users/humnaawan/repos/3D-galaxies-kavli/data/tng_highres/xy/features_28.csv'
@@ -125,5 +129,65 @@ then
                     --shape_datapath=${shape_datapath} \
                     --outdir=${outdir} --high_res \
                     --good_radius_feats \
-                    --triaxiality_based
+                    --triaxiality_based --plot_feature_dists
+fi
+# ------------------------------------------------------------------------------
+# no masses included; no a1a4; 3 classes; w/ tsne features
+if [ $allclasses_noa1a4_wtsne == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_3classes_wtsne/'
+    # classification
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res --plot_feature_dists \
+                    --no_2nd_order_feats --good_radius_feats \
+                    --add_tsne_feats --n_comps=3
+    # regression
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --no_2nd_order_feats --good_radius_feats --regress \
+                    --add_tsne_feats --n_comps=3
+fi
+# ------------------------------------------------------------------------------
+# 2 classes; no masses included; no a1a4; wtsne
+if [ $twoclasses_noa1a4_wtsne == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_2classes_wtsne/'
+    # classification
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --no_2nd_order_feats --good_radius_feats \
+                    --prolate_vs_not --add_tsne_feats --n_comps=2 --plot_feature_dists
+fi
+
+# ------------------------------------------------------------------------------
+# regress with triaxiality; no masses; no a1a4; wtsne
+if [ $regress_wT_noa1a4_wtsne == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_3classes_Tonly_wtsne/'
+    # regression
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --no_2nd_order_feats --good_radius_feats --regress \
+                    --triaxiality_based --add_tsne_feats --n_comps=2
+fi
+# ------------------------------------------------------------------------------
+# classify based on triaxiality; no masses; no a1a4; wtsne
+if [ $classify_wT_noa1a4_wtsne == 1 ];
+then
+    outdir=${base_outdir}'rf_tng_highres_we_2classes_T-based_wtsne/'
+    # regression
+    python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_rf.py \
+                    --features_file=${features_file} \
+                    --shape_datapath=${shape_datapath} \
+                    --outdir=${outdir} --high_res \
+                    --no_2nd_order_feats --good_radius_feats \
+                    --triaxiality_based --add_tsne_feats --n_comps=2
 fi

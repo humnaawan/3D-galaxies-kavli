5164fa64ce27eecb602b2471b6bbff5ad9a3cc12
Humna Awan <humna.awan@rutgers.edu>
2019-08-04 22:11:26 -0700

    Updated run script to get shape50.

--- runscripts/bash_get_tng_shapes.sh
+++ runscripts/bash_get_tng_shapes.sh
@@ -3,4 +3,4 @@
 data_dir='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/tng-100/'
 
 python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/run_get_shapes.py \
-                --data_dir=${data_dir}
+                --data_dir=${data_dir} --rdecider=50

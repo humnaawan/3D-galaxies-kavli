f965cf96f30023edd780b6ed951605201ddfc37f
Humna Awan <humna.awan@rutgers.edu>
2019-08-08 13:25:22 -0700

    Updated run script for the tng features.

--- runscripts/bash_get_tng_features.sh
+++ runscripts/bash_get_tng_features.sh
@@ -2,11 +2,13 @@
 
 summary_datapath='/Users/humnaawan/repos/3D-galaxies-kavli/data/tng_highres/xy/'
 shape_datapath='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/tng-100/'
+outdir='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/extracted_features/'
 
 python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/get_features.py \
                 --summary_datapath=${summary_datapath} \
-                --shape_datapath=${shape_datapath} --outdir=${summary_datapath}
+                --shape_datapath=${shape_datapath} --outdir=${outdir} \
+                --data_tag='xy'
 python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/get_features.py \
                 --summary_datapath=${summary_datapath} \
-                --shape_datapath=${shape_datapath} --outdir=${summary_datapath} \
-                --rdecider=50
+                --shape_datapath=${shape_datapath} --outdir=${outdir} \
+                --rdecider=50 --data_tag='xy'

41565d23a28f4121127f154e31d8fa96e93201ec
Humna Awan <humna.awan@rutgers.edu>
2019-08-09 04:37:34 -0700

    Updated bash script for one-proj tng data for get-features.

--- runscripts/bash_get_tng_features.sh
+++ runscripts/bash_get_tng_features.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-summary_datapath='/Users/humnaawan/repos/3D-galaxies-kavli/data/tng_highres/xy/'
-shape_datapath='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/tng-100/'
-outdir='/Users/humnaawan/repos/3D-galaxies-kavli/outputs/extracted_features/'
+base_dir='/Users/humnaawan/repos/3D-galaxies-kavli/'
+summary_datapath=${base_dir}'data/tng_highres/xy/'
 
+shape_datapath=${base_dir}'outputs/tng-100_z0.4_shape100/'
+outdir=${base_dir}'outputs/tng-100_z0.4_shape100_features/'
 python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/get_features.py \
                 --summary_datapath=${summary_datapath} \
                 --shape_datapath=${shape_datapath} --outdir=${outdir} \
                 --data_tag='xy'
-python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/get_features.py \
-                --summary_datapath=${summary_datapath} \
-                --shape_datapath=${shape_datapath} --outdir=${outdir} \
-                --rdecider=50 --data_tag='xy'
+
+shape_datapath=${base_dir}'outputs/tng-100_z0.4_shape50/'
+outdir=${base_dir}'outputs/tng-100_z0.4_shape50_features/'
+#python /Users/humnaawan/repos/3D-galaxies-kavli/runscripts/get_features.py \
+#                --summary_datapath=${summary_datapath} \
+#                --shape_datapath=${shape_datapath} --outdir=${outdir} \
+#                --rdecider=50 --data_tag='xy'
